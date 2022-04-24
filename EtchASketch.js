3dad616cba95fa9dc72510f5bbc56e4c3c7d934c
asnhck12 <ahussain93@live.co.uk>
2022-04-23 17:19:59 -0700

    Basic version of the project

---
+++ EtchASketch.js
@@ -0,0 +1,36 @@
+const container = document.getElementById("container");
+const resetButton = document.getElementById("resetButton");
+const div1 = document.querySelectorAll(".grid");
+var size = 16;
+
+makeGrid(size);
+
+resetButton.addEventListener("click", () => {
+    reset();
+});
+
+function makeGrid() {
+    for (let rows = 0;rows < size*size; rows++) {
+        const div = document.createElement('div');
+        div.classList.add('grid');
+        div.style.width = (960/size + "px");
+        div.style.height = (960/size + "px");
+        container.append(div);
+        
+    };
+    document.querySelectorAll(".grid").forEach((cell) => {
+            cell.addEventListener("mouseover", () => {
+                cell.classList.add("colouredGrid");
+            });
+        });
+};
+
+function reset() {
+    div1.forEach((div1) => {
+        div1.classList.remove("colouredGrid");
+    });
+    container.innerHTML = "";
+    size = prompt("What size?");
+    if (size > 64) {size= 64};
+    makeGrid();
+};
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 # Etch_A_Sketch
+=======
+An Etch-A-Sketch project.
+>>>>>>> 46b5e29 (Basic version of the project)
